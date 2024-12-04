c3de1431ce1cd86fccf57d24df92d8a7b44cb7fa
CarloCattano <freevibes@gmail.com>
2024-12-04 09:29:38 +0100

    Test ex08 code fix

--- ex08/fix_misc_dev.c
+++ ex08/fix_misc_dev.c
@@ -6,6 +6,7 @@
 #include <linux/slab.h>
 #include <linux/uaccess.h> // Required for copy_from_user
 
+// fixed license to GPL
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
@@ -27,7 +28,8 @@ static struct miscdevice myfd_device = {
 	.fops = &myfd_fops,
 };
 
-char str[PAGE_SIZE];
+static char str[PAGE_SIZE];
+static char *tmp;
 
 static int __init myfd_init(void)
 {
@@ -38,26 +40,47 @@ static int __init myfd_init(void)
 static void __exit myfd_cleanup(void)
 {
 	misc_deregister(&myfd_device);
+	printk(KERN_INFO "Cleaning up module.\n");
 }
 
 ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
 {
 	size_t t, i;
-	size_t len = strlen(str);
-	for (t = len - 1, i = 0; t < len; t--, i++) {
-		user[i] = str[t];
+
+	tmp = kmalloc(PAGE_SIZE, GFP_KERNEL);
+	// PAGE_SIZE is the size of a memory page in the kernel
+	// GFP_KERNEL is a flag that indicates the type of memory allocation
+
+	if (!tmp)
+		return -ENOMEM; // Not enough memory
+
+	for (t = strlen(str), i = 0; t > 0; t--, i++)
+		tmp[i] = str[t - 1];
+
+	tmp[i] = '\0';
+
+	if (copy_to_user(user, tmp, i)) {
+		kfree(tmp);
+		return -EFAULT; // Bad address
 	}
-	user[len] = '\0';
-	return simple_read_from_buffer(user, size, offs, user, len);
+
+	kfree(tmp);
+	return i;
 }
 
-ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
-		   loff_t *offs)
+static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size,
+			  loff_t *offs)
 {
-	ssize_t res =
-		simple_write_to_buffer(str, sizeof(str) - 1, offs, user, size);
+	ssize_t res;
+
+	if (size >= PAGE_SIZE)
+		return -EINVAL;
+
+	res = simple_write_to_buffer(str, PAGE_SIZE - 1, offs, user, size);
+
 	if (res < 0)
 		return res;
+
 	str[res] = '\0';
 	return res;
 }
